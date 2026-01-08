c9cf59f06825198cf711bac641da071829533a36
Programeerchacho <201045@gsf.nl>
2026-01-08 14:38:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAntwoord() {
     let feedback = document.getElementById("feedback");
 
     if (antwoord === "rivier") {
-        feedback.innerHTML = "Goed zo! 🎉 <br><a href='opdracht2.html'>Ga naar de volgende QR</a>";
+        feedback.innerHTML = "Goed zo! 🎉 
     } else {
         feedback.innerHTML = "Dat is niet goed, probeer opnieuw!";
     }

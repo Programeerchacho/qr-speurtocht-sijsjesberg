7b3ee34651bf7963c5b09f12630873256eb94674
Programeerchacho <201045@gsf.nl>
2026-01-08 13:51:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function checkAntwoord() {
+    let antwoord = document.getElementById("antwoord").value.toLowerCase();
+    let feedback = document.getElementById("feedback");
+
+    if (antwoord === "klok") {
+        feedback.innerHTML = "Goed zo! 🎉 <br><a href='opdracht2.html'>Ga naar de volgende QR</a>";
+    } else {
+        feedback.innerHTML = "Dat is niet goed, probeer opnieuw!";
+    }
+}
 
